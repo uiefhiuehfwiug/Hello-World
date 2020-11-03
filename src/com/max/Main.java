a98bdb745170ff7585a4f30deef8a0a174051da3
uiefhiuehfwiug <73860151+uiefhiuehfwiug@users.noreply.github.com>
2020-11-03 01:04:18 +0100

    Added test fork

--- src/com/max/Main.java
+++ src/com/max/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("My first Java Code on GitHub!");
+        System.out.println("Test Fork");
     }
 }
